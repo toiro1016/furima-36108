e62cb2efc13809d7a904e6825f456962d722c457
toiro1016 <86334987+toiro1016@users.noreply.github.com>
2021-07-23 16:32:38 +0900

    修正１

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,12 +1,13 @@
 FactoryBot.define do
   factory :item do
+    association :user
     name { 'test' }
     description { 'test' }
-    category_id { '2' }
-    status_id { '2' }
-    shipping_fee_id { '2' }
-    shipping_address_id { '2' }
-    delivery_day_id { '2' }
+    category_id { 2 }
+    status_id { 2 }
+    shipping_fee_id { 2 }
+    shipping_address_id { 2 }
+    delivery_day_id { 2 }
     price { 400 }
 
     after(:build) do |item|
--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -6,7 +6,14 @@ RSpec.describe Item, type: :model do
   end
 
   describe '商品出品' do
-    context '新規登録できない時' do
+    context '商品出品ができる時' do
+      it '商品名、商品説明、カテゴリー、商品の状態、配送料の負担情報、発送元、
+      発送までの日数、価格、商品画像が存在すれば登録できる' do
+        expect(@item).to be_valid
+      end
+    end 
+
+    context '商品出品ができない時' do
       it '商品名が空では登録できない' do
         @item.name = ''
         @item.valid?
@@ -18,27 +25,27 @@ RSpec.describe Item, type: :model do
         expect(@item.errors.full_messages).to include("Description can't be blank")
       end
       it 'カテゴリーが空では登録できない' do
-        @item.category_id = ''
+        @item.category_id = '1'
         @item.valid?
         expect(@item.errors.full_messages).to include("Category can't be blank")
       end
       it '商品状態が空では登録できない' do
-        @item.status_id = ''
+        @item.status_id = '1'
         @item.valid?
         expect(@item.errors.full_messages).to include("Status can't be blank")
       end
       it '配送料の負担情報が空では登録できない' do
-        @item.shipping_fee_id = ''
+        @item.shipping_fee_id = '1'
         @item.valid?
         expect(@item.errors.full_messages).to include("Shipping fee can't be blank")
       end
       it '発送元が空では登録できない' do
-        @item.shipping_address_id = ''
+        @item.shipping_address_id = '1'
         @item.valid?
         expect(@item.errors.full_messages).to include("Shipping address can't be blank")
       end
       it '発送までの日数が空では登録できない' do
-        @item.delivery_day_id = ''
+        @item.delivery_day_id = '1'
         @item.valid?
         expect(@item.errors.full_messages).to include("Delivery day can't be blank")
       end
