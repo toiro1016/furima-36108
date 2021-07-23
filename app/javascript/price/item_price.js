49914043334bb4a262309c43d9535c682d2faa82
toiro1016 <86334987+toiro1016@users.noreply.github.com>
2021-07-23 16:55:26 +0900

    修正２

--- app/javascript/price/item_price.js
+++ app/javascript/price/item_price.js
@@ -1,11 +1,8 @@
 window.addEventListener('load', () => {
     const addTaxDom = document.getElementById("add-tax-price");
     addTaxDom.innerHTML = (Math.floor(inputValue * 0.1));
-    console.log(addTaxDom);
     const profitNumber = document.getElementById("profit")
     const price_result = inputValue * 0.1
-    console.log(price_result)
     profitNumber.innerHTML = (Math.floor(inputValue - price_result));
-      console.log(profitNumber);
   })
 })
--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -25,27 +25,27 @@ RSpec.describe Item, type: :model do
         expect(@item.errors.full_messages).to include("Description can't be blank")
       end
       it 'カテゴリーが空では登録できない' do
-        @item.category_id = '1'
+        @item.category_id = 1
         @item.valid?
         expect(@item.errors.full_messages).to include("Category can't be blank")
       end
       it '商品状態が空では登録できない' do
-        @item.status_id = '1'
+        @item.status_id = 1
         @item.valid?
         expect(@item.errors.full_messages).to include("Status can't be blank")
       end
       it '配送料の負担情報が空では登録できない' do
-        @item.shipping_fee_id = '1'
+        @item.shipping_fee_id = 1
         @item.valid?
         expect(@item.errors.full_messages).to include("Shipping fee can't be blank")
       end
       it '発送元が空では登録できない' do
-        @item.shipping_address_id = '1'
+        @item.shipping_address_id = 1
         @item.valid?
         expect(@item.errors.full_messages).to include("Shipping address can't be blank")
       end
       it '発送までの日数が空では登録できない' do
-        @item.delivery_day_id = '1'
+        @item.delivery_day_id = 1
         @item.valid?
         expect(@item.errors.full_messages).to include("Delivery day can't be blank")
       end
@@ -54,7 +54,8 @@ RSpec.describe Item, type: :model do
         @item.valid?
         expect(@item.errors.full_messages).to include("Price can't be blank")
       end
-      it '価格が半角数値でなければ登録できない' do
+      it '価格が半角数値でなけれ
+      ば登録できない' do
         @item.price = '１２３４'
         @item.valid?
         expect(@item.errors.full_messages).to include('Price is not included in the list')
@@ -63,6 +64,11 @@ RSpec.describe Item, type: :model do
         @item.image = nil
         @item.valid?
         expect(@item.errors.full_messages).to include("Image can't be blank")
+      end
+      it '商品に紐づくユーザーが存在しない場合登録できない' do
+        @item.user = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include("User must exist")
       end
     end
   end
